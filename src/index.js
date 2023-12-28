fefccb612dd1c9cd6bb7f9c0b89acc12bc3d4e28
Liliya <6750lilia@gmail.com>
2023-12-28 11:37:47 +0300

    add browser router in project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/about",
+    element: <div>This is about page</div>
+  },
+  {
+    path: "/terms",
+    element: <div>This is terms page</div>
+  },
+  {
+    path: "/post/:id",
+    element: <div>This is posts page</div>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
