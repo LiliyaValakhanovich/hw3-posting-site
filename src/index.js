7b3a1bfec8e974b5679126c85497d26ba0d3d32a
Liliya <6750lilia@gmail.com>
2023-12-30 00:01:40 +0300

    delete comments

--- src/index.js
+++ src/index.js
@@ -13,12 +13,6 @@ import { Terms } from './pages/terms';
 import { Post } from './pages/post';
 
 const router = createBrowserRouter([
-  /*<Routes>
-    <Route path="/" element={<App/>} errorElement={<h1>Page was not found</h1>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/terms" element={<Terms/>}/>
-    <Route path="/post/:id" element={<Post/>}/>
-  </Routes>*/
   {
     path: "/start",
     element: <App/>,
