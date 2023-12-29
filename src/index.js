b3256d6545618da57e305b2d8aa9da8baf29c922
Liliya <6750lilia@gmail.com>
2023-12-30 00:00:56 +0300

    change url start page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 //import { BrowserRouter } from 'react-router-dom';
 import {
+  Navigate,
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom';
@@ -12,21 +13,35 @@ import { Terms } from './pages/terms';
 import { Post } from './pages/post';
 
 const router = createBrowserRouter([
+  /*<Routes>
+    <Route path="/" element={<App/>} errorElement={<h1>Page was not found</h1>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/terms" element={<Terms/>}/>
+    <Route path="/post/:id" element={<Post/>}/>
+  </Routes>*/
+  {
+    path: "/start",
+    element: <App/>,
+    errorElement:<h1>Page was not found</h1>
+  },
   {
     path: "/",
-    element: <App/>
+    element: <Navigate replace to="/start"/>
   },
   {
     path: "/about",
-    element: <About/>
+    element: <About/>,
+    errorElement:<h1>Page was not found</h1>
   },
   {
     path: "/terms",
-    element: <Terms/>
+    element: <Terms/>,
+    errorElement:<h1>Page was not found</h1>
   },
   {
     path: "/post/:id",
-    element: <Post/>
+    element: <Post/>,
+    errorElement:<h1>Page was not found</h1>
   }
 ])
 
